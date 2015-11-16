28da8df0fecac21c4c29e5254be9f97c156004ee
Andrew Hoover <andrew.hoover@mines.sdsmt.edu>
2015-11-16 16:52:11 -0700

    Python script to automatically change network to and from "internet mode"

---
+++ Source/Sprint3Code/network.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import fileinput
+import os
+
+if os.geteuid() != 0:
+	exit("Forgot the sudo :(")
+
+addComment = False
+removeComment = False
+for line in fileinput.input("/etc/network/interfaces2", inplace=True):
+	if line == "auto eth0\n":
+		addComment = True
+	if line == "# auto eth0\n":
+		removeComment = True
+	if addComment:	
+		print "# %s" % (line),
+	else:
+		if removeComment:
+			print "%s" % (line[2:]),
+		else:
+			print "%s" % (line),
+os.system("ifdown eth0")
+os.system("ifup eth0")
