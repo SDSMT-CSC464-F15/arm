1ba0e14dbd3345b4375fe16aa0e1058c1a8a830c
cnsorensen <christine.sorensen@mines.sdsmt.edu>
2015-12-01 10:52:29 -0700

    Updated log and mount script

--- Source/Sprint3Code/mountHomeDir.py
+++ Source/Sprint3Code/mountHomeDir.py
@@ -6,8 +6,8 @@ def Main():
 
 	for host in hosts:
 		if host != 'snow_white':
-			cmd = "ssh " + host + " 'mount -t nfs snow_white:/home /home'"
+			cmd = "ssh odroid@" + host + " 'mount -t nfs snow_white:/home /home'"
 			os.system( cmd )
 
-if __name__ == '__main__'
+if __name__ == '__main__':
 	Main()
